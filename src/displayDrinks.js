1ed6c5b7dc1abb6f329f9fa357eee22ff2e0893d
amir <amirazizihosein@gmail.com>
2023-02-10 22:25:34 +0330

    displayDrinks all drink with letter a

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -9,5 +9,20 @@ const displayDrinks = ({ drinks }) => {
     section.innerHTML = null
     return
   }
+  const newDrinks = drinks
+    .map((drinks) => {
+      const { idDrink: id, strDrink: name, strDrinkThumb: image } = drinks
+      return `<a href="drink.html">
+          <article class="cocktail" data-id="${id}">
+            <img src="${image}" alt="${name}" />
+            <h3>${name}</h3>
+          </article>
+        </a>`
+    })
+    .join("")
+  // hide loading
+  title.textContent = ""
+  section.innerHTML = newDrinks
+  return section
 }
 export default displayDrinks
--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -3,7 +3,6 @@ import displayDrinks from "./displayDrinks.js"
 const showDrinks = async (url) => {
   // fetch drinks
   const data = await fetchDrink(url)
-  console.log(data)
   // display drinks
   const section = await displayDrinks(data)
 }
