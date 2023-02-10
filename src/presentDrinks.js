2f804467fd0491168edf9baab98596ac51f81398
amir <amirazizihosein@gmail.com>
2023-02-11 00:50:54 +0330

    add single drink page

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,11 +1,13 @@
 import fetchDrink from "./fetchDrinks.js"
 import displayDrinks from "./displayDrinks.js"
 import { showLoading } from "./toggleLoading.js"
+import setDrink from "./setDrink.js"
 const showDrinks = async (url) => {
   showLoading()
   // fetch drinks
   const data = await fetchDrink(url)
   // display drinks
   const section = await displayDrinks(data)
+  if (section) setDrink(section)
 }
 export default showDrinks
